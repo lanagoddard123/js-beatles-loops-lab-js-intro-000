b7176b5c3e7d1f998c0e6848103056294e1ef96a
lanasgoddard <lanasgoddard@gmail.com>
2018-12-27 12:22:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return phrases;
 }
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+johnLennonFacts(facts){
+  while(facts.length>-1){
+    facts.push('!!!')
+  } return facts
+}
